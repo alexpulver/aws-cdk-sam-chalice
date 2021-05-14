c92df4c2c6f2a84b70196fc66df2e22e3ab14876
Alex Pulver <alex.pulver@gmail.com>
2021-05-14 16:06:05 +0300

    Fix build step in pipeline

--- pipeline/infrastructure.py
+++ pipeline/infrastructure.py
@@ -23,13 +23,17 @@ class Pipeline(cdk.Stack):
             action_name='GitHub', output=source_artifact,
             connection_arn='arn:aws:codestar-connections:eu-west-1:807650736403:connection/1f244295-871f-411f-afb1-e6ca987858b6',
             owner='alexpulver', repo='aws-cdk-sam-chalice', branch='future')
-        synth_action = pipelines.SimpleSynthAction.standard_npm_synth(
-            source_artifact=source_artifact,
-            cloud_assembly_artifact=cloud_assembly_artifact)
 
+        synth_action = pipelines.SimpleSynthAction(
+            source_artifact=source_artifact,
+            cloud_assembly_artifact=cloud_assembly_artifact,
+            install_commands=['npm install', 'pip install -r requirements.txt'],
+            synth_command='npx cdk synth')
+
+        # TODO: Use the CDK CLI version number from package.json dynamically
         cdk_pipeline = pipelines.CdkPipeline(
             self, 'CdkPipeline', source_action=source_action, synth_action=synth_action,
-            cloud_assembly_artifact=cloud_assembly_artifact)
+            cdk_cli_version='1.103.0', cloud_assembly_artifact=cloud_assembly_artifact)
 
         test_env = cdk.Environment(account='807650736403', region='eu-west-1')
         test_stage = Stage(self, f'{APPLICATION_NAME}PipelineTestStage', env=test_env)
