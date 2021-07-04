a2dfe77b631c607ec1a9dba0eb3a829a3876ca82
Alex Pulver <alex.pulver@gmail.com>
2021-07-04 10:55:59 +0300

    Add safety tool to check dependencies for known security vulnerabilities

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -12,5 +12,6 @@ isort --settings-path .isort.cfg --check --diff "${_targets[@]}"
 mypy --config-file .mypy.ini api  # Splitting commands due to https://github.com/python/mypy/issues/4008
 mypy --config-file .mypy.ini database monitoring pipeline app.py config.py stages.py
 pylint --rcfile .pylintrc "${_targets[@]}"
+safety check -r api/runtime/requirements.txt -r requirements.txt -r requirements-dev.txt
 
 coverage run --rcfile .coveragerc -m unittest discover --start-directory tests
