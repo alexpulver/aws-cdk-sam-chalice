0ee2929f186871c67fe1b412caf1a02a30dd1a95
Alex Pulver <alex.pulver@gmail.com>
2021-07-19 01:04:18 +0300

    Switch to modern CDK Pipelines API

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,6 @@ import json
 from pathlib import Path
 from typing import Any
 
-from aws_cdk import aws_codepipeline as codepipeline
-from aws_cdk import aws_codepipeline_actions as codepipeline_actions
 from aws_cdk import core as cdk
 from aws_cdk import pipelines
 
@@ -17,41 +15,32 @@ class Pipeline(cdk.Stack):
     def __init__(self, scope: cdk.Construct, id: str, **kwargs: Any):
         super().__init__(scope, id, **kwargs)
 
-        source_artifact = codepipeline.Artifact()
-        cloud_assembly_artifact = codepipeline.Artifact()
-
-        source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
-            action_name="GitHub",
-            output=source_artifact,
-            # pylint: disable=line-too-long
-            connection_arn="arn:aws:codestar-connections:eu-west-1:807650736403:connection/1f244295-871f-411f-afb1-e6ca987858b6",
-            owner="alexpulver",
-            repo="aws-cdk-sam-chalice",
-            branch="future",
+        codepipeline_source = pipelines.CodePipelineSource.connection(
+            "alexpulver/aws-cdk-sam-chalice",
+            "future",
+            connection_arn="arn:aws:codestar-connections:eu-west-1:807650736403:connection/1f244295-871f-411f-afb1-e6ca987858b6"
         )
-
-        synth_action = pipelines.SimpleSynthAction(
-            source_artifact=source_artifact,
-            cloud_assembly_artifact=cloud_assembly_artifact,
-            install_commands=[
-                "pyenv local 3.7.10",
-                "./scripts/install-deps.sh",
-            ],
-            test_commands=["./scripts/run-tests.sh"],
-            synth_command="npx cdk synth",
+        synth_commands = [
+            "pyenv local 3.7.10",
+            "./scripts/install-deps.sh",
+            "./scripts/run-tests.sh",
+            "npx cdk synth",
+        ]
+        synth_shell_step = pipelines.ShellStep(
+            "Synth",
+            input=codepipeline_source,
+            commands=synth_commands
         )
-
-        cdk_pipeline = pipelines.CdkPipeline(
+        codepipeline = pipelines.CodePipeline(
             self,
-            "CdkPipeline",
-            source_action=source_action,
-            synth_action=synth_action,
-            single_publisher_per_type=True,
-            cdk_cli_version=Pipeline._get_cdk_cli_version(),
-            cloud_assembly_artifact=cloud_assembly_artifact,
+            "CodePipeline",
+            cli_version=Pipeline._get_cdk_cli_version(),
+            cross_account_keys=True,
+            publish_assets_in_parallel=False,
+            synth=synth_shell_step,
         )
 
-        self._add_pre_prod_stage(cdk_pipeline)
+        self._add_pre_prod_stage(codepipeline)
 
     @staticmethod
     def _get_cdk_cli_version() -> str:
@@ -61,22 +50,16 @@ class Pipeline(cdk.Stack):
         cdk_cli_version = str(package_json["devDependencies"]["aws-cdk"])
         return cdk_cli_version
 
-    def _add_pre_prod_stage(self, cdk_pipeline: pipelines.CdkPipeline) -> None:
+    def _add_pre_prod_stage(self, codepipeline: pipelines.CodePipeline) -> None:
         pre_prod_env = cdk.Environment(account="807650736403", region="eu-west-1")
         pre_prod_stage = PreProd(self, f"{APP_NAME}-PreProd", env=pre_prod_env)
 
         api_endpoint_url_env_var = f"{APP_NAME.upper()}_API_ENDPOINT_URL"
-        pre_prod_smoke_test_outputs = {
-            api_endpoint_url_env_var: cdk_pipeline.stack_output(
-                pre_prod_stage.api_endpoint_url
-            )
-        }
-        pre_prod_smoke_test_commands = [f"curl ${api_endpoint_url_env_var}"]
-        pre_prod_smoke_test_action = pipelines.ShellScriptAction(
-            action_name="SmokeTest",
-            use_outputs=pre_prod_smoke_test_outputs,
-            commands=pre_prod_smoke_test_commands,
+        smoke_test_commands = [f"curl ${api_endpoint_url_env_var}"]
+        smoke_test_shell_step = pipelines.ShellStep(
+            "SmokeTest",
+            env_from_cfn_outputs={api_endpoint_url_env_var: pre_prod_stage.api_endpoint_url},
+            commands=smoke_test_commands,
         )
 
-        pre_prod_cdk_stage = cdk_pipeline.add_application_stage(pre_prod_stage)
-        pre_prod_cdk_stage.add_actions(pre_prod_smoke_test_action)
+        codepipeline.add_stage(pre_prod_stage, post=[smoke_test_shell_step])
