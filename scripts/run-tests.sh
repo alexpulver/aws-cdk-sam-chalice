03eceb1e99cd5e02636e543e65d13c1f80fb90b1
Alex Pulver <alex.pulver@gmail.com>
2021-08-02 23:22:35 +0300

    Ignore safety warning for Coverage.py, Snyk reports clean

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -9,10 +9,16 @@ bandit --recursive "${targets[@]}"
 black --check --diff "${targets[@]}"
 flake8 --config .flake8 "${targets[@]}"
 isort --settings-path .isort.cfg --check --diff "${targets[@]}"
-mypy --config-file .mypy.ini api database monitoring  # Splitting commands due to https://github.com/python/mypy/issues/4008
+# mypy: split commands due to https://github.com/python/mypy/issues/4008
+mypy --config-file .mypy.ini api database monitoring
 mypy --config-file .mypy.ini app.py deployment.py pipeline.py
 pylint --rcfile .pylintrc "${targets[@]}"
-safety check -r api/runtime/requirements.txt -r requirements.txt -r requirements-dev.txt
+# safety: ignore coverage 5.5 (41002) report, no issues per Snyk: https://snyk.io/advisor/python/coverage
+safety check \
+  -i 41002 \
+  -r api/runtime/requirements.txt \
+  -r requirements.txt \
+  -r requirements-dev.txt
 
 PYTHONPATH="${PWD}/api/runtime" \
   coverage run --source "${PWD}" --omit ".venv/*,tests/*" -m unittest discover -v -s tests
