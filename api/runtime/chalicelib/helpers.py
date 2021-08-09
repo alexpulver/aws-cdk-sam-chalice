0e778d164f2ab8943b7772a3bda89cb0bfb0cc03
Alex Pulver <alex.pulver@gmail.com>
2021-08-09 16:00:52 +0300

    Move the branch content to alexpulver/usermanagement-backend

--- api/runtime/chalicelib/helpers.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from chalicelib import users
-
-
-def init_users_repository() -> users.UsersRepository:
-    dynamodb_database = users.DynamoDBDatabase(os.environ["TABLE_NAME"])
-    users_repository = users.UsersRepository(database=dynamodb_database)
-    return users_repository
